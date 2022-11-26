d0c20f29ab615726706a8f290473375bf74114e2
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-11-26 08:55:27 +0100

    Removed all the docstring

--- src/main.py
+++ src/main.py
@@ -1,7 +1,3 @@
-"""
-Main entry point for the application.
-"""
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -9,11 +5,9 @@ app = FastAPI()
 
 @app.get("/")
 async def index():
-    """Index route"""
     return {"hello": "world"}
 
 
 @app.get("/{param}")
 async def test_param(param: str):
-    """This tests that params are passed correctly"""
     return {"param": param}
